f6d42f0f1607f12b943ccdbacd3326e31c47a2dd
Aashay Desai <aashay.desai@clever.com>
2014-11-05 12:33:53 -0800

    Use beaker middleware for sessions to show an actual flow with redirect

--- python/server.py
+++ python/server.py
@@ -1,10 +1,11 @@
-from bottle import route, run, request, template
-
 import base64
 import json
 import os
 import requests
 import urllib
+
+from bottle import app, redirect, request, route, run, template
+from beaker.middleware import SessionMiddleware
 
 CLIENT_ID = os.environ['CLIENT_ID']
 CLIENT_SECRET = os.environ['CLIENT_SECRET']
@@ -20,6 +21,14 @@ CLEVER_API_BASE = 'https://api.clever.com'
 
 DISTRICT_ID = os.environ['DISTRICT_ID']
 
+# Use the bottle session middleware to store an object to represent a "logged in" state.
+session_opts = {
+    'session.type': 'memory',
+    'session.cookie_expires': 300,
+    'session.auto': True
+}
+myapp = SessionMiddleware(app(), session_opts)
+
 @route('/')
 def index():
     encoded_string = urllib.urlencode({
@@ -29,11 +38,10 @@ def index():
         'scope': 'read:user_id read:sis',
         'district_id': DISTRICT_ID
     })
-    return template("<h1>Sign In!<br/><br/> \
+    return template("<h1>Login!<br/><br/> \
         <a href='https://clever.com/oauth/authorize?" + encoded_string +
         "'><img src='http://assets.clever.com/sign-in-with-clever/sign-in-with-clever-small.png'/></a></h1>"
     )
-
 
 @route('/oauth')
 def oauth():
@@ -41,7 +49,7 @@ def oauth():
     scope = request.query.scope
 
     payload = {
-    'code': code,
+        'code': code,
         'grant_type': 'authorization_code',
         'redirect_uri': REDIRECT_URI
     }
@@ -52,6 +60,7 @@ def oauth():
     }
 
     resp = requests.post(CLEVER_OAUTH_URL, data=json.dumps(payload), headers=headers).json()
+
     token = resp['access_token']
 
     bearer_headers = {
@@ -60,9 +69,25 @@ def oauth():
 
     result = requests.get(CLEVER_API_BASE + '/me', headers=bearer_headers).json()
 
+    print json.dumps(result, sort_keys=True, indent=4, separators=(',', ': '))
+
     nameObject = result['data']['name']
-    return template("You are now logged in as {{name}}", name=nameObject['first'] + ' ' + nameObject['last'])
+    session = request.environ.get('beaker.session')
+    session['nameObject'] = nameObject
+
+    redirect('/app')
+
+
+@route('/app')
+def app():
+    session = request.environ.get('beaker.session')
+    if 'nameObject' in session:
+        nameObject = session['nameObject']
+        return template("You are now logged in as {{name}}", name=nameObject['first'] + ' ' + nameObject['last'])
+    else:
+        return "You must be logged in to see this page! Click <a href='/'>here</a> to log in."
+
 
 if __name__ == '__main__':
-    run(host='localhost', port=PORT)
+    run(app=myapp, host='localhost', port=PORT)
 
