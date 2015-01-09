61d5538f36a5124c9f22022b7c5adc8d4c81d6cc
Taylor Singletary <taylor.singletary@clever.com>
2015-01-09 13:52:38 -0800

    Improve comments and explanation of flow

--- php/index.php
+++ php/index.php
@@ -1,8 +1,15 @@
 <?php
 /**
  * This script demonstrates Clever Instant Login, using OAuth 2.0 for token acquistion & request authentication.
+ * When run in a server context, process_incoming_requests() will listen for incoming HTTP requests on the specified host and port after evaluating pertinent configuration options.
+ * Requests to "/" or any paths besides "/oauth" will be served a simple HTML page. If you've configured a Clever district ID, a "Sign in with Clever" link will be displayed. When that link is followed and authentication is completed on clever.com, the user will be redirected to this script's OAuth 2.0 redirect flow.
+ * Requests to "/oauth" will be treated as OAuth 2.0 client redirects and passed to our process_client_redirect() function to perform a few brief steps:
+ * - The exchange code value passed to our redirect URI is exchanged for an OAuth 2.0 bearer token representing the current user
+ * - The bearer token will be used in a request to Clever's "/me" API resource to discover additional information about the current user
+ * - The current user's information will be displayed in HTML.
  */
 
+// By default, stop on all errors. In a production application, you may want to use only E_WARNING or otherwise silence with ~E_ALL
 error_reporting(E_ALL);
 
 // Handle incoming requests if we're running in a web server context
@@ -19,7 +26,7 @@ if($_SERVER && array_key_exists('REQUEST_URI', $_SERVER)) {
  */
 function set_options(array $override_options = NULL) {
   $options = array(
-    # Obtain your Client ID and secret from your Clever developer dashboard at https://account.clever.com/partner/applications
+    // Obtain your Client ID and secret from your Clever developer dashboard at https://account.clever.com/partner/applications
     'client_id' => getenv('CLEVER_CLIENT_ID'),
     'client_secret' => getenv('CLEVER_CLIENT_SECRET'),
     'port' => getenv('CLEVER_PORT'),
